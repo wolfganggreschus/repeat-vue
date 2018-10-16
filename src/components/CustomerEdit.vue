914b8aab4311a55e1669c390b3e6ecd88b8c9543
thestarcash <wolfgang@greschus.de>
2018-10-16 21:48:27 +0200

    no comment

---
+++ src/components/CustomerEdit.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>I#m here</p>
+        {{ customer }}
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      customer: 'Schweitzer'
+    }
+  }
+}
+</script>
